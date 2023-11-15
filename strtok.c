25216ef6bacff6c2989619d9a3006ede5b3e4446
MohammedGebril <mjbryl72@gmail.com>
2023-11-15 17:13:23 +0200

    0

--- strtok.c
+++ strtok.c
@@ -8,13 +8,13 @@
 int check_char(const char *s,char ch)
 {
 
-  for (int h=0;  s[h] != '\0'; h++)
-    {
-      if (ch == s[h])
-	return (1);
-      continue;
-    }
-  return (0);
+for (int h=0;  s[h] != '\0'; h++)
+{
+if (ch == s[h])
+return (1);
+continue;
+}
+return (0);
 }
 
 /**
@@ -26,41 +26,41 @@ int check_char(const char *s,char ch)
 
 char *stringtoken(char *delimiter, const char *string)
 {
-  int h;
-  char *tok_c;
-  char *tok_n;
+int h;
+char *tok_c;
+char *tok_n;
 
-  if (string != NULL)
-    tok_n = string;
-  tok_c = tok_n;
-  if (tok_c == NULL)
-    return (NULL);
-  for ( h = 0; tok_c[h] != '\0'; h++)
-    {
-      if (check_char(tok_c[h], delimiter) == 0)
-	break;
-    }
-  if (tok_n[h] == '\0' || tok_n[h] == '#')
-    {
-      tok_n = NULL;
-      return (NULL);
-    }
-  tok_c = tok_n + h;
-  tok_n = tok_c;
-  for (h = 0; tok_n[h] != '\0'; h++)
-    {
-      if (check_char(tok_n[h], delimiter) == 1)
-	break;
-    }
-  if (tok_n[h] == '\0')
-    tok_n = NULL;
-  else
-    {
-      tok_n[h] = '\0';
-      tok_n = tok_n + h + 1;
-      if (*tok_n == '\0')
-	tok_n = NULL;
-    }
-  return (tok_c);
+if (string != NULL)
+tok_n = string;
+tok_c = tok_n;
+if (tok_c == NULL)
+return (NULL);
+for ( h = 0; tok_c[h] != '\0'; h++)
+{
+if (check_char(tok_c[h], delimiter) == 0)
+break;
+}
+if (tok_n[h] == '\0' || tok_n[h] == '#')
+{
+tok_n = NULL;
+return (NULL);
+}
+tok_c = tok_n + h;
+tok_n = tok_c;
+for (h = 0; tok_n[h] != '\0'; h++)
+{
+if (check_char(tok_n[h], delimiter) == 1)
+break;
+}
+if (tok_n[h] == '\0')
+tok_n = NULL;
+else
+{
+tok_n[h] = '\0';
+tok_n = tok_n + h + 1;
+if (*tok_n == '\0')
+tok_n = NULL;
+}
+return (tok_c);
 }
 
